ecb83537f65dca77d119348c359de4205d9ef464
riya7794 <shahiriya699@gmail.com>
2026-05-17 09:37:41 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
+import time
 
-st.set_page_config(page_title="AI CV Analyzer", layout="wide", page_icon="📄")
-
+st.set_page_config(page_title=" Welcome to CV Analyzer", layout="wide", page_icon="📄")
+st.snow()
 st.title("🎯 Smart CV Analysis & Screening Dashboard")
 st.subheader("Upload resumes to extract insights, match job descriptions, and rank candidates.")
